19240e04eadb6655f2ae27806e7ce1a9f490bed0
klaus <klausnu@me.com>
2021-03-18 22:08:02 +0100

    final

--- src/com/Klaus/jdbcNotes/jdbcNotes.java
+++ src/com/Klaus/jdbcNotes/jdbcNotes.java
@@ -12,32 +12,17 @@ public class jdbcNotes {
     public static void main(String[] args) {
         int userId = chooseUser();
         while (true) {
-        printNotes();
-        newNoteFromScanner();
+        printNotes(userId);
+        newNoteFromScanner(userId);
         }
     }
 
-    private static int chooseUser() {
-        int userId = 0;
-        printUsers();
-        //TODO choose User
-        //scanner input id (int)
-        //
 
 
-        return userId;
-    }
-
-    private static void printUsers() {
-        //TODO
-
-
-    }
-
-    private static void newNoteFromScanner() {
+    private static void newNoteFromScanner(int userId) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bitte neue Notiz eingeben:");
+        System.out.println("Please insert new note:");
         String note = sc.nextLine();
 
         Connection conn = null;
@@ -45,8 +30,16 @@ public class jdbcNotes {
             conn = DriverManager.getConnection(url, usr, pwd);
             //System.out.println("Connection established");
 
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO Notes (text, user) VALUES (?,?)");
+            ps.setString(1, note);
+            ps.setInt(2, userId);
+            ps.executeUpdate();
+
+            /* BEWARE: Do not concat SQL queries, possible SQL injection
             Statement stmt = conn.createStatement();
-            stmt.executeUpdate("INSERT INTO Notes (text) VALUES (\"" + note + "\")");
+            stmt.executeUpdate("INSERT INTO Notes (text, user) VALUES (\"" + note + "\","  + userId + ")");
+            */
+
             }
          catch (SQLException se) {
             se.printStackTrace();
@@ -57,29 +50,41 @@ public class jdbcNotes {
                 } catch (SQLException se){
                     se.printStackTrace();
                 }
-
             }
-
         }
-
     }
 
-    private static void printNotes() {
+    private static void printNotes(int userId) {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, usr, pwd);
-            System.out.println("Connection established");
+            //System.out.println("Connection established");
 
+
+            /* BEWARE: Do not concat SQL queries, possible SQL injection
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT id, text, created FROM notes ORDER BY created ASC");
+            ResultSet rs = stmt.executeQuery("SELECT notes.id id, notes.text text, notes.created created, users.name name " +
+                    "FROM notes JOIN users ON users.id = notes.user " +
+                    "WHERE users.id = " + userId +
+                    " ORDER BY created ASC");
+             */
+
+            PreparedStatement ps = conn.prepareStatement("SELECT notes.id AS id, notes.text AS text, " +
+                    "notes.created AS created, users.name AS name " +
+                    "FROM notes JOIN users ON users.id = notes.user " +
+                    "WHERE users.id = ? ORDER BY created ASC");
+            ps.setInt(1, userId);
+
+            ResultSet rs = ps.executeQuery();
 
             while (rs.next()){
                 int id = rs.getInt("id");
                 String text = rs.getString("text");
                 Date dateCreated = rs.getDate("created");
                 Time timeCreated = rs.getTime("created");
+                String name = rs.getString("name");
 
-                System.out.println("note " + id + " from " + dateCreated.toString() + " " + timeCreated.toString() + ": " + text);
+                System.out.println("note " + id + " from " + name + " at " + dateCreated.toString() + " " + timeCreated.toString() + ": " + text);
             }
         } catch (SQLException se) {
             se.printStackTrace();
@@ -90,9 +95,43 @@ public class jdbcNotes {
                 } catch (SQLException se){
                     se.printStackTrace();
                 }
-
             }
+        }
+    }
 
+    private static int chooseUser() {
+        Scanner sc = new Scanner(System.in);
+        int userId = 0;
+        printUsers();
+        System.out.println("Please type user ID:");
+        userId = sc.nextInt();
+        return userId;
+    }
+
+    private static void printUsers() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, usr, pwd);
+            //System.out.println("Connection established");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT id, name FROM users ORDER BY id ASC");
+
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("Name");
+                System.out.println(name + " (ID " + id + ")");
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try{
+                    conn.close();
+                } catch (SQLException se){
+                    se.printStackTrace();
+                }
+            }
         }
     }
 }
